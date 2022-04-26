51732909fc317f19ebb6b60f52dc37ce1fa68e6a
AlexFilh0 <filhoa172@gmail.com>
2022-04-26 11:01:37 -0300

    Quase pronto

--- controle.py
+++ controle.py
@@ -1,29 +1,97 @@
 from PyQt5 import uic,QtWidgets
-import mysql.connector
+import sqlite3
+
+from banco import criacao_banco
+
 
 numero_id = 0
 
-banco = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    passwd="diamond10",
-    database="pvn_v2"
-)
+banco = sqlite3.connect('pvn_banco.bd')
 
 
 def cadastrar():
     nome = acolhido.edtNome.text()
+    rg = acolhido.edtRG.text()
     cpf = acolhido.edtCPF.text()
+    nascimento = acolhido.edtNascimento.text()
+    estado_civil = acolhido.edtECivil.text()
+    qtd_filhos = acolhido.edtFilho.text()
+    trabalho = acolhido.edtTrabalho.text()
+    posto_saude = acolhido.edtPSaude.text()
+    lazer = acolhido.edtLazer.text()
+    substancia_favorita = acolhido.edtSubst.text()
+    usoAno = acolhido.edtTUso.text()
+    religiao = acolhido.edtReligiao.text()
+    responsavel = acolhido.edtResponsavel.text()
+    rg_responsavel = acolhido.edtReRG.text()
+    cpf_responsavel = acolhido.edtReCPF.text()
+    vinculo = acolhido.edtVinculo.text()
+    entrada = acolhido.edtEntrada.text()
+    saida = acolhido.edtSaida.text()
+    obs = acolhido.edtObs.text()
 
-    comando_SQL = "INSERT INTO acolhidos (nome, cpf) VALUES (%s,%s)"
-    dados = (str(nome), str(cpf))
     cursor = banco.cursor()
-    cursor.execute(comando_SQL, dados)
+    cursor.execute('''INSERT INTO acolhidos (nome, rg, cpf, nascimento, estado_civil,
+                    qtd_filhos, trabalho, posto_saude, lazer, substancia_favorita,
+                    uso_desde_idade, religiao, responsavel, rg_responsavel, cpf_responsavel,
+                    vinculo_responsavel, entrada, saida, obs)
+                    VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)''', (str(nome), str(rg), str(cpf), str(nascimento),
+                                      str(estado_civil), int(qtd_filhos), str(trabalho),
+                                      str(posto_saude), str(lazer), str(substancia_favorita),
+                                      int(usoAno), str(religiao), str(responsavel), str(rg_responsavel),
+                                      str(cpf_responsavel), str(vinculo), str(entrada), str(saida), str(obs)))
     banco.commit()
 
-    acolhido.edtNome.setText("")
-    acolhido.edtCPF.setText("")
-    acolhido.lblAviso.setText(f"{nome} CADASTRADO COM SUCESSO")
+def medicamentos_abrir():
+    cadMedicamento.show()
+    c = banco.cursor()
+
+
+    comando_SQL = ("""SELECT * FROM medicamentos""")
+    c.execute(comando_SQL)
+    lidos = c.fetchall()
+    cadMedicamento.tabelaCadastro.setRowCount(len(lidos))
+    cadMedicamento.tabelaCadastro.setColumnCount(4)
+
+    for i in range(len(lidos)):
+        for j in range(4):
+            cadMedicamento.tabelaCadastro.setItem(i, j, QtWidgets.QTableWidgetItem(str(lidos[i][j])))
+
+
+def cadastrar_medicamento():
+    cadMedicamento.show()
+
+    nome = cadMedicamento.edtNomeMed.text()
+    espec = cadMedicamento.edtEsp.text()
+    obs = cadMedicamento.edtObs.text()
+
+    c = banco.cursor()
+
+    c.execute('''INSERT INTO medicamentos (nome, especificacoes, obs)
+                VALUES (?, ?, ?)''',(str(nome), str(espec), str(obs)))
+    banco.commit()
+
+
+def entradas_abrir():
+    cadEntradaMedicamento.show()
+
+def cadastrar_entrada():
+    id_medicamento = cadEntradaMedicamento.edtIdMed.text()
+    qtd_entrada = cadEntradaMedicamento.edtQtdMed.text()
+    data_entrada = cadEntradaMedicamento.edtDataMed.text()
+    obs = cadEntradaMedicamento.edtObs.text()
+
+    c = banco.cursor()
+
+    c.execute("""INSERT INTO entradas_medicamentos (id_medicamento, qtd_entrada, data_entrada, obs)
+                VALUES (?, ?, ?, ?)""", (id_medicamento, qtd_entrada, data_entrada, obs))
+
+def tratamentos_abrir():
+    tratamento.show()
+
+def cadastrar_tratamento():
+
+    print('erros aqui não')
 
 def verCadastro():
     cadastrado.show()
@@ -42,24 +110,70 @@ def verCadastro():
             cadastrado.tabela.setItem(i, j, QtWidgets.QTableWidgetItem(str(lidos[i][j])))
 
 def excluirDado():
-    linha = cadastrado.tabela.currentRow()
-    cadastrado.tabela.removeRow(linha)
+    excluir = cadastrado.edtExcluir.text()
+    cadastrado.tabela.removeRow(int(excluir)-1)
 
     cursor = banco.cursor()
-    cursor.execute("SELECT id FROM acolhidos")
-    dados_lidos = cursor.fetchall()
-    valor_id = dados_lidos[linha][0]
-    cursor.execute("DELETE FROM acolhidos WHERE id="+ str(valor_id))
 
+    cursor.execute("DELETE FROM acolhidos WHERE id_acolhido="+ str(excluir))
+    banco.commit()
 
-app=QtWidgets.QApplication([])
+    #Colocar label para atualizar
+
+def visualizarDado():
+    visual.show()
+
+    idVer = cadastrado.edtVisualizar.text()
+    cursor = banco.cursor()
+
+    cursor.execute("""SELECT nome, rg, cpf, nascimento,
+     estado_civil, qtd_filhos, trabalho, posto_saude,
+     lazer, substancia_favorita, uso_desde_idade,
+     religiao, responsavel, rg_responsavel,
+     cpf_responsavel, vinculo_responsavel,
+     entrada, saida, obs FROM acolhidos WHERE id_acolhido=?""", (str(idVer)))
+
+    infoGet = cursor.fetchone()
+    visual.lblNome.setText(infoGet[0])
+    visual.lblRG.setText(infoGet[1])
+    visual.lblCPF.setText(infoGet[2])
+    visual.lblNascimento.setText(infoGet[3])
+    visual.lblECivil.setText(infoGet[4])
+    visual.lblFilhos.setText(str(infoGet[5]))
+    visual.lblTrabalho.setText(infoGet[6])
+    visual.lblPSaude.setText(infoGet[7])
+    visual.lblLazer.setText(infoGet[8])
+    visual.lblSubst.setText(infoGet[9])
+    visual.lblIdade.setText(str(infoGet[10]))
+    visual.lblReligiao.setText(infoGet[11])
+    visual.lblResponsavel.setText(infoGet[12])
+    visual.lblReRG.setText(infoGet[13])
+    visual.lblReCPF.setText(infoGet[14])
+    visual.lblVinculo.setText(infoGet[15])
+    visual.lblEntrada.setText(infoGet[16])
+    visual.lblSaida.setText(infoGet[17])
+    visual.lblOBS.setText(infoGet[18])
+
+app = QtWidgets.QApplication([])
 
 acolhido=uic.loadUi("formAcolhido.ui")
 cadastrado=uic.loadUi("formCadastrado.ui")
 
 acolhido.btnCadastrar.clicked.connect(cadastrar)
 acolhido.btnVer.clicked.connect(verCadastro)
+acolhido.btnMedicamento.clicked.connect(medicamentos_abrir)
+acolhido.btnEntradaMed.clicked.connect(entradas_abrir)
+acolhido.btnTratamento.clicked.connect(tratamentos_abrir)
+
 cadastrado.btnExcluir.clicked.connect(excluirDado)
+cadastrado.btnVisualizar.clicked.connect(visualizarDado)
+
+cadMedicamento.btnCadastrarMed.clicked.connect(cadastrar_medicamento)
+
+cadEntradaMedicamento.btnCadastrarMed.clicked.connect(cadastrar_entrada)
+
+tratamento.btnCadastrarTratamento.clicked.connect(cadastrar_tratamento)
+
 
 acolhido.show()
 app.exec()
